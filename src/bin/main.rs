d002c9762517dd5335fb231ada195bb31efc67c4
Matthew Blake <matthewblake0625@gmail.com>
2018-08-13 14:29:50 -0400

    Began work on ThreadPool to limit max threads.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,6 @@
+extern crate hello_server;
+use hello_server::ThreadPool;
+
 use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
@@ -6,11 +9,12 @@ use std::thread;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        thread::spawn(|| {
+        pool.execute(|| {
             handle_connection(stream);
         });
     }
